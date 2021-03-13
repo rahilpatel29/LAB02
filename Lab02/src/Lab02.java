6b8c9eaeb93bc3935d77aa5476511984dc65403d
patel <patel@10.0.0.124>
2021-03-13 03:12:24 -0800

    third changes

--- Lab02/src/Lab02.java
+++ Lab02/src/Lab02.java
@@ -1,15 +1,14 @@
-
+// Rahil patel
 // jainish patel
-public class Lab02 {
-
-
-
+public class Lab02 
+{
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
-		System.out.println("SacStateID - This line was added by rahil patel.");
-		System.out.println("SacStateID - This line was added by jainish patel.");
+		System.out.println("301675634 - This line was added by rahil patel.");
+		System.out.println(" - This line was added by jainish patel.");
 	}
 
 }
