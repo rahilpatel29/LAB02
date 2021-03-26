77039669c9833caf15adc05a76b257cc0d068683
Paneri Patel <58495243+Paneripatel@users.noreply.github.com>
2021-03-26 10:46:12 -0700

    Update Lab02.java

--- Lab02/src/Lab02.java
+++ Lab02/src/Lab02.java
@@ -9,7 +9,7 @@ public class Lab02 {
 		// Add a new line with your information
 		System.out.println("SacStateID - This line was added by rahil patel.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - ");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
